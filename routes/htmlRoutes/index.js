3b257879a3285936015111a508c2d208351c21a1
Jaspreet Khela <jaspreetkhela@Jaspreets-MacBook-Pro.local>
2021-09-08 04:37:04 -0400

    Implemented GET * for index.html file

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -4,14 +4,14 @@ const path = require('path');
 // Import Express.js router module
 const router = require('express').Router();
 
-// GET API route for the index.html page
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-  });
-
 // GET API route for the notes.html page
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+// GET API route for the index.html page
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 // Export router object
